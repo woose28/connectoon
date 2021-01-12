73a411794a739bb785c1eee3f46e28f78258f029
woose28 <dldntmdsla@naver.com>
2021-01-12 17:51:07 +0900

    Modify footer

--- src/Footer/footer.js
+++ src/Footer/footer.js
@@ -1,27 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
+import Logo from '../assets/component/logo';
 import './footer.css';
 
+const Container = styled.div`
+    width: 1920px;
+    height: 300px;
+    border-top: 1px solid #DFDFDF; 
+    position: relative;
+    margin-top: 120px;
+`;
+
+const Column = styled.div`
+    width: 810px;
+    display: flex;
+    flex-direction: column;
+    margin-top: 50px;
+    position: absolute;
+    left: 402px;
+`;
+
+const TxtContainer = styled.div`
+    white-space: pre-wrap;
+    margin-top: 39px;
+    font: normal normal 500 12px/30px Noto Sans CJK KR;
+    color: #aaaaaa;
+`;
+
 const Footer = () => {
-    const service_name = "CONNECTOON";
-    const service_basicInfo = "CEO: 김주영ㅣ 셀스토리주식회사 | 사업자등록번호: 471-88-01548ㅣ";
-    const service_address = "사업장주소: 경기도 평택시 청북읍 토진 2길 92";
+    const service_basicInfo = "CEO: 김주영ㅣ 셀스토리주식회사 | 사업자등록번호: 471-88-01548ㅣ 사업장주소: 경기도 평택시 청북읍 토진 2길 92";
     const service_contact = "E-mail: support@sellstory.co.kr";
+    const service_copyRight = "Copyrightⓒ 2020 sellstory. Inc All Rights Reserved";
+    const service_warning = "Connectoon은 작가와 광고주를 연결해주는 플랫폼으로써 광고, 판매 등에 대한 책임을 지지 않습니다.";
 
     return(
-        <div className="footer-container">
-            <div className="footer-service-name">
-                {service_name}   
-            </div>
-            <div className="footer-service-info">
-                {service_basicInfo}
-            </div>
-            <div className="footer-service-info">
-                {service_address}
-            </div>
-            <div className="footer-service-info">
-                {service_contact}
-            </div>
-        </div>
+        <Container>
+            <Column>
+                <Logo c_color={"#aaaaaa"} d_color={"#aaaaaa"}/>
+                <TxtContainer>
+                    {service_basicInfo+"\n"+service_contact}
+                </TxtContainer>
+                <TxtContainer>
+                    {service_copyRight+"\n"+service_warning}
+                </TxtContainer>
+            </Column>
+        </Container>
     );
 }
 
