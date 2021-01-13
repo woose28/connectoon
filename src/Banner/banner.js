94d1ead137ab09266e46a4d0980ac0c60506b13e
woose28 <dldntmdsla@naver.com>
2021-01-13 20:58:12 +0900

    Add responsive and animation

--- src/Banner/banner.js
+++ src/Banner/banner.js
@@ -1,29 +1,61 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import Icon_svg from '../assets/img/icon.svg'
 import Banner_img_svg from '../assets/img/banner_img.svg';
 
 const Container = styled.div`
-    width: 1920px;
+    width: 100%;
     height: 589px;
     background: #5759FF 0% 0% no-repeat padding-box;
     opacity: 1;
     display: flex;
     flex-direction: row;
     position: relative;
+
+    @media (max-width: 1200px) {
+        width: 1200px
+    }
 `;
+
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0
+  }
+  to {
+    opacity: 1
+  }
+`;
+
 
 const Column = styled.div`
     display: flex;
     flex-direction: column;
     position: absolute;
     top: ${props => props.top};
-    left: ${props => props.left};
+    left: ${props => props.left };
+    right: ${props => props.right};
+    bottom: ${props => props.bottom};
+
+    
+    @media (max-width: 1200px) {
+        left: ${props => props.isFirst? "110px": "auto"};
+        right: ${props => props.isFirst?  "auto": "90px"}
+    }
+
+    /*
+    @media (max-width: 950px){
+        top: ${props => props.isFirst? "16.1544%": "auto"};
+    }
+    */
 `;
 
 const Icon = styled.img`
     width: 83px;
     height: 83px;
+
+    animation: ${fadeIn};
+    animation-duration: 2s;
 `;
 
 const TxtContainer = styled.div`
@@ -33,6 +65,9 @@ const TxtContainer = styled.div`
     font: ${props => props.font};
     white-space: pre-wrap;
     color: #ffffff;
+    
+    animation: ${fadeIn};
+    animation-duration: 2s;
 `;
 const TxtInsta = styled.div`
     display: inline-block;
@@ -42,13 +77,17 @@ const TxtInsta = styled.div`
 `;
 
 const BannerImg = styled.img`
-
+    width: 450px;
+    height: 483px;
+    
+    animation: ${fadeIn};
+    animation-duration: 2s;
 `;
 
 const Banner = () => {
     return (
         <Container>
-            <Column top={"95.15px"} left={"380px"}>
+            <Column isFirst={true} top={"95.15px"} bottom={undefined} left={undefined} right={"57.5vw"}>
                 <Icon src={Icon_svg} alt="icon"/>
                 <TxtContainer 
                     width={"400px"} height={"182px"} marginTop={"23.31px"} 
@@ -62,7 +101,7 @@ const Banner = () => {
                     {"나에게 딱 맞는 인스타툰 작가를\n빠르고 쉽게 찾을 수 있습니다."}
                 </TxtContainer>
             </Column>
-            <Column top={"105.8px"} left={"1058.09px"}>
+            <Column isFirst={false} top={undefined} bottom={"0px"} left={"55.15vw"} right={undefined}>
                 <BannerImg src={Banner_img_svg} alt="banner_img"/>
             </Column>
         </Container>
